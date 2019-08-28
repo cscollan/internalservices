3feeaa1c9d39867c0c410665ffaf8ff1d114b8af
Cian Scollan <cscollan@gamblingcommission.gov.uk>
2019-08-28 14:35:38 +0100

    revisions

--- app/controllers/transactionalServicesController.js
+++ app/controllers/transactionalServicesController.js
@@ -10,19 +10,27 @@ exports.transactional_services_detail_page_get = function (req, res) {
   console.log('detail_page')
   var slug = req.params.id;
   var detail_page;
+  var list_of_transactional_services;
+
   console.log(slug)
   Promise.all([
       client.getEntries({
         'content_type': 'publicationTransactionalContent',
         'fields.slug': slug
+      }),
+      client.getEntries({
+        'content_type': 'publicationTransactionalContent',
+        order: 'fields.title'
       })
     ])
-    .then(([n]) => {
+    .then(([n, o]) => {
       detail_page = n,
-    
+      list_of_transactional_services = o
+
   console.log(n)
       res.render('transactional_services/detail_page', {
-        detail_page
+        detail_page, 
+        list_of_transactional_services
       });
     })
     .catch(error => {
@@ -37,16 +45,18 @@ exports.transactional_services_get = function (req, res) {
     console.log('get services')
 
     var transactional_services;
+    
   
     Promise.all([
         client.getEntries({
           'content_type': 'publicationTransactionalContent',        
           order: 'fields.title'
         })
+       
       ])
       .then(([n]) => {
-        transactional_services = n,
-      
+        transactional_services = n
+              
         res.render('transactional_services/index', {
             transactional_services
         });
@@ -56,3 +66,4 @@ exports.transactional_services_get = function (req, res) {
       });
 
 }
+
--- app/controllers/webServicesController.js
+++ app/controllers/webServicesController.js
@@ -10,19 +10,27 @@ exports.web_services_detail_page_get = function (req, res) {
   console.log('detail_page')
   var slug = req.params.id;
   var detail_page;
+  var list_of_web_services;
+
   console.log(slug)
   Promise.all([
       client.getEntries({
         'content_type': 'publicationWebServicesContent',
         'fields.slug': slug
+      }),
+      client.getEntries({
+        'content_type': 'publicationWebServicesContent',
+        order: 'fields.title'
       })
     ])
-    .then(([n]) => {
+    .then(([n, o]) => {
       detail_page = n,
-    
+      list_of_web_services = o
+
   console.log(n)
       res.render('web_services/detail_page', {
-        detail_page
+        detail_page, 
+        list_of_web_services
       });
     })
     .catch(error => {
@@ -37,16 +45,18 @@ exports.web_services_get = function (req, res) {
     console.log('get services')
 
     var web_services;
+    
   
     Promise.all([
         client.getEntries({
           'content_type': 'publicationWebServicesContent',        
           order: 'fields.title'
         })
+       
       ])
       .then(([n]) => {
-        web_services = n,
-      
+        web_services = n
+              
         res.render('web_services/index', {
             web_services
         });
@@ -56,3 +66,4 @@ exports.web_services_get = function (req, res) {
       });
 
 }
+
